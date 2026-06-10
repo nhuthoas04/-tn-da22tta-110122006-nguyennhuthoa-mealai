626f299d97f2cf2ace370fd6563ce900cd05d509
nhuthoa <nhuthoas04@gmail.com>
2026-06-10 21:50:17 +0700

    [Tính năng] Yêu thích và thích công thức

---
+++ backend/src/modules/recipes/entities/favorite.entity.ts
@@ -0,0 +1,31 @@
+import {
+    Entity, PrimaryGeneratedColumn, Column, CreateDateColumn,
+    ManyToOne, JoinColumn, Unique,
+} from 'typeorm';
+import { User } from '../../auth/entities/user.entity';
+import { Recipe } from './recipe.entity';
+
+@Entity('favorites')
+@Unique(['userId', 'recipeId'])
+export class Favorite {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ type: 'uuid' })
+    userId: string;
+
+    @Column({ type: 'uuid' })
+    recipeId: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    // --- Relations ---
+    @ManyToOne(() => User, (user) => user.favorites, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'userId' })
+    user: User;
+
+    @ManyToOne(() => Recipe, (recipe) => recipe.favorites, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'recipeId' })
+    recipe: Recipe;
+}
